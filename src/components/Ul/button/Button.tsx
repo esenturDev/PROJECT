b8a57ac50e6d8afec1c9cc729600c5e856e57756
esenturDev <nurlanuuluesentur@gmail.com>
2024-03-04 11:26:16 +0600

    added TRELLO PROJECT

--- src/components/Ul/button/Button.tsx
+++ src/components/Ul/button/Button.tsx
@@ -1,19 +1,19 @@
 import { FC, ReactNode } from "react";
 import styled from "styled-components";
 export const Button: FC<{
-  children: ReactNode;
-  onClick: () => void;
-}> = ({children, onClick}) => {
-  return <Buttons onClick={onClick}>{children}</Buttons>
-}
+	children: ReactNode;
+	onClick: () => void;
+}> = ({ children, onClick }) => {
+	return <Buttons onClick={onClick}>{children}</Buttons>;
+};
 
 const Buttons = styled.button`
-  width: 400px;
-  height: 51px;
-  border-radius: 3px;
-  background-color: #26b326;
-  color: #fff;
-  font-size: 17px;
-  border: none;
-  
-`
+	cursor: pointer;
+	width: 258px;
+	height: 31px;
+	border-radius: 3px;
+	background-color: blue;
+	color: #fff;
+	font-size: 14px;
+	border: none;
+`;
--- src/components/Ul/input/Input.tsx
+++ src/components/Ul/input/Input.tsx
@@ -1,27 +1,39 @@
-import { FC } from "react"
+import { FC } from "react";
 import styled from "styled-components";
 
 const Input: FC<{
-  type: string;
-  value: string;
-  setData: (value: string) => void;
-  placeholder: string;
-}> = ({type, value, setData, placeholder}) => {
-  return <Inputs placeholder={placeholder} type={type} value={value} onChange={(e) => setData(e.target.value)} />
-}
+	type: string;
+	value: string;
+	setData: (value: string) => void;
+	placeholder: string;
+}> = ({ type, value, setData, placeholder }) => {
+	return (
+		<Inputs
+			placeholder={placeholder}
+			type={type}
+			value={value}
+			onChange={(e) => setData(e.target.value)}
+		/>
+	);
+};
 
-export default Input
+export default Input;
 
 const Inputs = styled.input`
-  width: 385px;
-  height: 51px;
-  border-radius: 3px;
-  border: none;
-  box-shadow: 0px 0px 10px black;
-  &:active {
-    border: 2px solid red;
-  }
-  padding-left: 15px;
-  color: #403b3b;
-  font-size: 18px;
-`
+	width: 240px;
+	height: 29px;
+	border-radius: 3px;
+	border: none;
+	box-shadow: 0px 0px 2px black;
+	&:hover {
+		border: 2px solid #a3a3fd93;
+		background-color: #a3a3fd52;
+		/* outline: blue; */
+	}
+	/* &:active {
+		border: none;
+	} */
+	padding-left: 15px;
+	color: #403b3b;
+	font-size: 12px;
+`;
